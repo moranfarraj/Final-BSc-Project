4e5b855cd747b319f03d5418f605e4c1b13ef464
HabebNawatha <habeb.naw@outlook.com>
2023-03-01 12:48:14 +0200

    GatheringOfGamers

--- app/src/main/java/com/example/gatheringofgamers/MainActivity.java
+++ app/src/main/java/com/example/gatheringofgamers/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

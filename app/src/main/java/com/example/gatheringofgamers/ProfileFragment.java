164cb6b179811b5ed1adddde8b1c6830989d75f1
moranfarraj <morianis247@gmail.com>
2023-05-15 14:46:13 +0300

    Upload photo from Google photos

--- app/src/main/java/com/example/gatheringofgamers/ProfileFragment.java
+++ app/src/main/java/com/example/gatheringofgamers/ProfileFragment.java
@@ -1,9 +1,18 @@
 package com.example.gatheringofgamers;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,13 +24,19 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class ProfileFragment extends Fragment {
 
     private static final String ARG_USER_ID = "user_id";
+    private static final int GALLERY_REQUEST_CODE = 123; // You can choose any integer here
+
 
     private ViewPager viewPager;
     private ProfilePagerAdapter pagerAdapter;
     private FirebaseFirestore db;
+    private ImageView profile_img;
     private String username;
 
     private String userId;
@@ -55,6 +70,7 @@ public class ProfileFragment extends Fragment {
         viewPager = view.findViewById(R.id.view_pager);
         pagerAdapter = new ProfilePagerAdapter(getChildFragmentManager(), userId);
         viewPager.setAdapter(pagerAdapter);
+        profile_img = view.findViewById(R.id.profile_picture);
 
         TabLayout tabLayout = view.findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
@@ -77,6 +93,32 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        // Declare the editProfilePictureButton
+        Button editProfilePictureButton = view.findViewById(R.id.editProfilePictureButton);
+        editProfilePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, GALLERY_REQUEST_CODE);
+            }
+        });
+
         return view;
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case GALLERY_REQUEST_CODE:
+                if(resultCode == Activity.RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    profile_img.setImageURI(selectedImage);
+                    // Use the uri to upload it to Firebase if you want
+                }
+        }
+    }
+
+
+
 }

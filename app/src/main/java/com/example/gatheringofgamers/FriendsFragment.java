24ee68a9a0c080fff2a16412b709d3ee40cd5577
habeb <habeb.naw@outlook.com>
2023-05-17 14:20:03 +0300

    doesnt work 100% but when going back to allies fragment it goes to tab 0.

--- app/src/main/java/com/example/gatheringofgamers/FriendsFragment.java
+++ app/src/main/java/com/example/gatheringofgamers/FriendsFragment.java
@@ -1,6 +1,7 @@
 package com.example.gatheringofgamers;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -41,6 +42,8 @@ public class FriendsFragment extends Fragment implements FriendsAdapter.ViewHold
     private String username;
     private TabLayout tabLayout;
     RecyclerView.Adapter adapter;
+    private SharedPreferences preferences;
+
     CollectionReference usersRef;
     List<User> users;
     private String userId;
@@ -77,6 +80,8 @@ public class FriendsFragment extends Fragment implements FriendsAdapter.ViewHold
         setView(view);
         tabLayout = view.findViewById(R.id.friendList_layout);
         handleTabSelection(0,view);
+        preferences = requireContext().getSharedPreferences("MyPrefs", this.getContext().MODE_PRIVATE);
+
 //        Query query;
 //        query = db.collection("friendRequests")
 //                .whereEqualTo("from",userId);
@@ -108,6 +113,45 @@ public class FriendsFragment extends Fragment implements FriendsAdapter.ViewHold
 
                     });
         return view;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int selectedTabPosition = preferences.getInt("SelectedTabPosition", 0);
+
+        // Set the TabLayout to display the first tab
+        tabLayout.selectTab(tabLayout.getTabAt(0));
+
+        // Update the selectedTabPosition variable if needed
+        selectedTabPosition = 0;
+
+        // Perform any necessary operations based on the selected tab
+
+        // Add listeners to handle tab selection events
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == 0) {
+                    handleTabSelection(tab.getPosition(),view);
+                } else if (tab.getPosition() == 1) {
+                    handleTabSelection(tab.getPosition(),view);
+                } else if (tab.getPosition() == 2) {
+                    handleTabSelection(tab.getPosition(),view);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // Handle tab unselection events
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Handle tab reselection events
+            }
+        });
     }
     private void handleTabSelection(int position,View view) {
         // Perform actions based on the selected tab position
